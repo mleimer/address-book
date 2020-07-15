af4f1b58ce3f4e6bd603e5754bd887b2f813135b
Martin Leimer <martin.leimer@zuehlke.com>
2020-07-15 13:29:34 +0800

    refactor: remove React.StrictMode checks, as Material UI does not yet fully support it (https://github.com/mui-org/material-ui/issues/13394)

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+    /*
+     * Remove React.StrictMode until Material UI has found a fix for
+     * https://github.com/mui-org/material-ui/issues/13394
+     *
+     * <React.StrictMode>
+     *     <App/>
+     * </React.StrictMode>
+     */
+    <App/>,
     document.getElementById('root')
 );
