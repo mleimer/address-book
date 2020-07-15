afc2bbb03e3d662de20d61d2c239b169063e32c7
Martin Leimer <martin.leimer@zuehlke.com>
2020-07-15 22:58:42 +0800

    test: add additional tests for NavBar

--- src/features/NavBar/NavBar.test.js
+++ src/features/NavBar/NavBar.test.js
@@ -2,17 +2,54 @@ import React from 'react';
 import {render, screen} from '../../test/testUtils';
 import NavBar from './NavBar';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {fireEvent} from '@testing-library/react';
+
+
+const navBarId = 'nav-bar';
+const searchFieldId = 'search-field';
+const addressBookTabId = 'address-book-tab';
+const settingsTabId = 'settings-tab';
 
 describe('<NavBar />', () => {
-    test('it should mount within Router component', () => {
+    test('it should mount within Router component and Address Book and Settings Tab', () => {
         render(
             <Router>
                 <NavBar/>
             </Router>
         );
 
-        const navBar = screen.getByTestId('nav-bar');
+        const navBar = screen.getByTestId(navBarId);
+        const addressBookTab = screen.getByTestId(addressBookTabId);
+        const settingsTab = screen.getByTestId(settingsTabId);
 
         expect(navBar).toBeInTheDocument();
+        expect(addressBookTab).toBeInTheDocument();
+        expect(addressBookTab).toHaveTextContent('Address book');
+        expect(settingsTab).toBeInTheDocument();
+        expect(settingsTab).toHaveTextContent('Settings');
     });
+
+    test('should show search field on Address book tab, but not on Settings tab', () => {
+        const {getByTestId, queryByTestId} = render(
+            <Router>
+                <NavBar/>
+            </Router>
+        );
+
+        let searchField = getByTestId(searchFieldId);
+        expect(searchField).toBeInTheDocument();
+
+        const settingsTab = getByTestId(settingsTabId);
+        fireEvent.click(settingsTab);
+
+        searchField = queryByTestId(searchFieldId);
+        expect(searchField).not.toBeInTheDocument();
+
+        const addressBookTab = getByTestId(addressBookTabId);
+        fireEvent.click(addressBookTab);
+
+        searchField = getByTestId(searchFieldId);
+        expect(searchField).toBeInTheDocument();
+    });
+
 });
